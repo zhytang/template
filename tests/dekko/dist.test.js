53ec4cb10a0ec34b18b7a083109e0725bacb7f7f
tangzy <tangzy@raysdata.com>
2022-03-15 18:45:36 +0800

    test(dekko): 格式

--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -3,17 +3,17 @@ const chalk = require('chalk');
 
 $('dist')
   .isDirectory()
-  .hasFile('antd2-with-locales.js')
-  .hasFile('antd2-with-locales.min.js')
+  .hasFile('antd-with-locales.js')
+  .hasFile('antd-with-locales.min.js')
   .hasFile('antd2.css')
-  .hasFile('antd2.min.css')
+  // .hasFile('antd2.min.css')
   .hasFile('antd2.js')
-  .hasFile('antd2.min.js')
-  .hasFile('antd2.less')
-  .hasFile('antd2.dark.less')
-  .hasFile('antd2.dark.css')
-  .hasFile('antd2.compact.less')
-  .hasFile('antd2.compact.css')
+  // .hasFile('antd2.min.js')
+  .hasFile('antd.less')
+  .hasFile('antd.dark.less')
+  .hasFile('antd2.dark2.css')
+  .hasFile('antd.compact.less')
+  .hasFile('antd.compact.css')
   .hasFile('dark-theme.js');
 
 // eslint-disable-next-line no-console
