b8ddb5af5d9252c7e63a40df49110bd17a5350f7
tangzy <tangzy@raysdata.com>
2022-03-15 17:22:14 +0800

    test(components): 单元测试

--- components/_util/__tests__/wave.test.js
+++ components/_util/__tests__/wave.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Wave from '../wave';
-import ConfigProvider from '../../config-provider';
+import ConfigProvider from 'antd/lib/config-provider';
 import mountTest from '../../../tests/shared/mountTest';
 import { sleep } from '../../../tests/utils';
 
