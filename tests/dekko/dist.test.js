9d0b3615698a5503a8e8a6487aea17be9310aeb3
tangzy <tangzy@raysdata.com>
2022-03-16 11:23:59 +0800

    test(tests): dist检查

--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -5,16 +5,19 @@ $('dist')
   .isDirectory()
   .hasFile('antd-with-locales.js')
   .hasFile('antd-with-locales.min.js')
-  .hasFile('antd2.css')
-  // .hasFile('antd2.min.css')
-  .hasFile('antd2.js')
-  // .hasFile('antd2.min.js')
   .hasFile('antd.less')
   .hasFile('antd.dark.less')
-  .hasFile('antd2.dark.css')
   .hasFile('antd.compact.less')
-  .hasFile('antd2.compact.css')
   .hasFile('dark-theme.js');
+
+$('dist/@zhytang')
+  .isDirectory()
+  .hasFile('antd2.css')
+  .hasFile('antd2.min.css')
+  .hasFile('antd2.js')
+  .hasFile('antd2.min.js')
+  .hasFile('antd2.dark.css')
+  .hasFile('antd2.compact.css');
 
 // eslint-disable-next-line no-console
 console.log(chalk.green('✨ `dist` directory is valid.'));
